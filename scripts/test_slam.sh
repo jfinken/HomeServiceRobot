351d8365da0258b76f23ac8001ed8038f4a95a85
Josh Finken <josh dot finken at protonmail>
2021-04-11 12:54:43 -0600

    adding teleop

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -4,11 +4,17 @@
 # export TURTLEBOT_GAZEBO_WORLD_FILE="/home/jfinken/projects/udacity/robotics-software-engineer/HomeServiceRobot/worlds/Q802.world"
 # xterm  -e  " gazebo " &
 # sleep 5
+
 xterm  -e  " source /opt/ros/noetic/setup.bash; roscore" & 
 sleep 5
+
 # launches gazebo and rviz
 xterm -e " roslaunch home_service_robot world.launch" &
 sleep 5
+
 # TODO: use launch file?
-xterm -e " rosrun gmapping slam_gmapping scan:=scan _base_frame:=chassis"
+xterm -e " rosrun gmapping slam_gmapping scan:=scan _base_frame:=chassis" &
 sleep 5
+
+# teleop
+xterm -e " rosrun teleop_twist_keyboard teleop_twist_keyboard.py"
