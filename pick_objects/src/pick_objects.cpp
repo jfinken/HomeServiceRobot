82dd57dbdb09c1ee295e2cc10ff107ee82c92631
Josh Finken <josh dot finken at protonmail>
2021-04-13 20:37:49 -0600

    Adding multiple goals to the pick_objects node; adding significant yaw and xy_goal_tolerances.

--- pick_objects/src/pick_objects.cpp
+++ pick_objects/src/pick_objects.cpp
@@ -28,30 +28,40 @@ int main(int argc, char** argv){
   goal1.target_pose.header.stamp = ros::Time::now();
 
   // Define a position and orientation for the robot to reach
-  goal1.target_pose.pose.position.x = -6.2;
-  goal1.target_pose.pose.position.y = -6.0;
-  goal1.target_pose.pose.orientation.z = 0.9;
-  goal1.target_pose.pose.orientation.w = 0.14;
+  goal1.target_pose.pose.position.x = -6.51;
+  goal1.target_pose.pose.position.y = -5.19;
+  //goal1.target_pose.pose.orientation.z = -0.296;
+  goal1.target_pose.pose.orientation.w = 0.95;
 
   //---------------------------------------------------------------------------
-  // Declare pickup zone #2
+  // Declare interim goal A
   //---------------------------------------------------------------------------
   move_base_msgs::MoveBaseGoal goal2;
-  // set up the frame parameters
   goal2.target_pose.header.frame_id = "map";
   goal2.target_pose.header.stamp = ros::Time::now();
+  goal2.target_pose.pose.position.x = -7.16;
+  goal2.target_pose.pose.position.y = -6.75;
+  goal2.target_pose.pose.orientation.z = 0.84;
+  goal2.target_pose.pose.orientation.w = 0.5327;
+  
+  //---------------------------------------------------------------------------
+  // Declare pickup zone 2
+  //------------------------------------------------------------l---------------
+  move_base_msgs::MoveBaseGoal goal3;
+  goal3.target_pose.header.frame_id = "map";
+  goal3.target_pose.header.stamp = ros::Time::now();
+  goal3.target_pose.pose.position.x = -5.5;
+  goal3.target_pose.pose.position.y = 3.7;
+  goal3.target_pose.pose.orientation.w = 0.7;
 
-  goal2.target_pose.pose.position.x = -5.5;
-  goal2.target_pose.pose.position.y = 3.7;
-  goal2.target_pose.pose.orientation.w = 0.7;
-
-  // More difficult 
+  // More difficult pickup zone 2
   //goal2.target_pose.pose.position.x = -14.0;
   //goal2.target_pose.pose.position.y = -6.0;
   //goal2.target_pose.pose.orientation.z = 0.72;
   //goal2.target_pose.pose.orientation.w = 0.70;
-  
-  // Send the goal position and orientation for the robot to reach
+
+  // Send goals
+  // TODO: DRY-out the below result checking 
   ROS_INFO("Sending goal #1");
   ac.sendGoal(goal1);
 
@@ -60,21 +70,41 @@ int main(int argc, char** argv){
 
   // Check if we've reached pickup zone 1, if so proceed to 2. 
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
-    ROS_INFO("The q802 robot successfully reached pickup zone #1");
-
-    // sleep before sending next goal
-    ros::Duration(7.0).sleep();
-
-    ROS_INFO("Sending goal #2");
-    ac.sendGoal(goal2);
-
-    // Wait an infinite time for the results
-    ac.waitForResult();
-    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-      ROS_INFO("The q802 robot successfully reached pickup zone #2");
+    ROS_INFO("The q802 robot successfully reached pickup zone 1");
   }
-  else
+  else {
     ROS_INFO("The base failed to move to goal #1.  Exiting...");
+    return 1;
+  }
+
+  // sleep before sending next goal
+  ros::Duration(2.0).sleep();
+
+  ROS_INFO("Sending interim goal A");
+  ac.sendGoal(goal2);
+
+  // Wait an infinite time for the results
+  ac.waitForResult();
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    ROS_INFO("The q802 robot successfully reached interim goal A");
+  else {
+    ROS_INFO("The q802 robot failed to move to interim goal A.  Exiting...");
+    return 1;
+  }
+  
+  // sleep before sending next goal
+  ros::Duration(2.0).sleep();
+
+  ROS_INFO("Sending pickup zone 2");
+  ac.sendGoal(goal3);
+  
+  ac.waitForResult();
+  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    ROS_INFO("The q802 robot successfully reached pickup zone 2");
+  else {
+    ROS_INFO("The q802 robot failed to move to pickup zone 2.  Exiting...");
+    return 1;
+  }
 
   // sleep before exiting...
   ros::Duration(5.0).sleep();
